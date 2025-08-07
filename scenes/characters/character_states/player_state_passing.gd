5096af84366eddae90ebf4430d65702a9e976999
j <urls3921@gmail.com>
2025-08-06 21:58:11 -0400

    upload

---
+++ scenes/characters/character_states/player_state_passing.gd
@@ -0,0 +1,29 @@
+class_name PlayerStatePassing
+extends PlayerState
+
+func _enter_tree() -> void:
+	animation_player.play("kick")
+	player.velocity = Vector2.ZERO
+	SoundPlayer.play(SoundPlayer.Sound.PASS)
+
+func on_animation_complete() -> void:
+	var pass_target := state_data.pass_target
+	if pass_target == null:
+		pass_target = find_teammate_in_view()
+	if pass_target == null:
+		ball.pass_to(ball.position + player.heading * player.speed)
+	else:
+		ball.pass_to(pass_target.position + pass_target.velocity)
+	transition_state(Player.State.MOVING)
+
+func find_teammate_in_view() -> Player:
+	var players_in_view := teammate_detection_area.get_overlapping_bodies()
+	var teammates_in_view := players_in_view.filter(
+		func(p: Player): return p != player
+	)
+	teammates_in_view.sort_custom(
+		func(p1: Player, p2: Player): return p1.position.distance_squared_to(player.position) < p2.position.distance_squared_to(player.position)
+	)
+	if teammates_in_view.size() > 0:
+		return teammates_in_view[0]
+	return null
---
+++ scenes/game_manager/game_manager.gd
@@ -0,0 +1,64 @@
+extends Node
+
+const DURATION_IMPACT_PAUSE := 100
+const DURATION_GAME_SEC := 2 * 60
+
+enum State {IN_PLAY, SCORED, RESET, KICKOFF, OVERTIME, GAMEOVER}
+
+var countries : Array[String] = ["FRANCE", "USA"]
+var current_state : GameState = null
+var player_setup : Array[String] = ["FRANCE", ""]
+var score : Array[int] = [0, 0]
+var state_factory := GameStateFactory.new()
+var time_left : float
+var time_since_paused := Time.get_ticks_msec()
+
+func _init() -> void:
+	process_mode = ProcessMode.PROCESS_MODE_ALWAYS
+
+func _ready() -> void:
+	time_left = DURATION_GAME_SEC
+	GameEvents.impact_received.connect(on_impact_received.bind())
+	switch_state(State.RESET)
+
+func _process(_delta: float) -> void:
+	if get_tree().paused and Time.get_ticks_msec() - time_since_paused > DURATION_IMPACT_PAUSE:
+		get_tree().paused = false
+
+func switch_state(state: State, data: GameStateData = GameStateData.new()) -> void:
+	if current_state != null:
+		current_state.queue_free()
+	current_state = state_factory.get_fresh_state(state)
+	current_state.setup(self, data)
+	current_state.state_transition_requested.connect(switch_state.bind())
+	current_state.name = "GameStateMachine: " + str(state)
+	call_deferred("add_child", current_state)
+	
+func is_coop() -> bool:
+	return player_setup[0] == player_setup[1]
+
+func is_single_player() -> bool:
+	return player_setup[1].is_empty()
+
+func is_game_tied() -> bool:
+	return score[0] == score[1]
+
+func is_time_up() -> bool:
+	return time_left <= 0
+
+func get_winner_country() -> String:
+	assert(not is_game_tied())
+	return countries[0] if score[0] > score[1] else countries[1]
+
+func increase_score(country_scored_on: String) -> void:
+	var index_country_scoring := 1 if country_scored_on == countries[0] else 0
+	score[index_country_scoring] += 1
+	GameEvents.score_changed.emit()
+
+func has_someone_scored() -> bool:
+	return score[0] > 0 or score[1] > 0
+
+func on_impact_received(_impact_position: Vector2, is_high_impact: bool) -> void:
+	if is_high_impact:
+		time_since_paused = Time.get_ticks_msec()
+		get_tree().paused = true
